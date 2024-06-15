40745ade5e1376a91f37b48c45ca8ad2d2545bc0
JeenathBandi <167793445+JeenathBandi@users.noreply.github.com>
2024-06-15 16:20:45 +0530

    Solution

--- src/components/LanguageFilterItem/index.js
+++ src/components/LanguageFilterItem/index.js
@@ -2,13 +2,22 @@
 import './index.css'
 
 const LanguageFilterItem = props => {
-  const {languageFiltersData} = props
+  const {languageFiltersData, onGetFilter, gotSelected} = props
   const {id, language} = languageFiltersData
+  const onSelectBtn = () => onGetFilter(id)
+
+  const btnRedstyle = gotSelected && 'red-btn'
+
   return (
-    <button className="btn" type="button">
+    <button
+      className={`btn ${btnRedstyle}`}
+      type="button"
+      onClick={onSelectBtn}
+    >
       {language}
     </button>
   )
 }
 
 export default LanguageFilterItem
+
